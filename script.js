1d95623cced29546d919cc46fae31c948a9bc58c
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-28 17:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+var fontsize = document.getElementById("fontsize");
+var fontcolor = document.getElementById("fontcolor");
+
+var mycookies = document.cookie.split("; ")
+mycookies.map((item)=>{
+	var cookieList = item.split("=");
+	var myfontsize = cookieList[1];
+	var myfontcolor = cookieList[3];
+
+	fontsize.value = myfontsize;
+	fontcolor.value = myfontcolor;
+})
+
+function onSubmit(event){
+	document.cookie = `fontsize=${fontsize.value}`;
+	document.cookie = `fontcolor=${fontcolor.value}`;
+	event.preventDefault();
+}
+
